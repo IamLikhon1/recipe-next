1d80d906ad13e91fa88e77fe7932525e98d8dc73
IamLikhon1 <likhonadnan55@gmail.com>
2024-01-17 09:40:30 +0600

    fix search button

--- src/component/SearchBar/SearchBar.jsx
+++ src/component/SearchBar/SearchBar.jsx
@@ -2,7 +2,7 @@ const SearchBar = () => {
     return (
         <div className="max-w-7xl mx-auto">
             <div>
-                <input type="text" className="border-2 focus:outline-none py-4 px-16 rounded-md border-[#07332F] font-bold ml-3" placeholder="Search Recipe By Title" />
+                <input type="text" className="border-2 focus:outline-none py-4 px-10 lg:px-16 rounded-md border-[#07332F] font-bold ml-3" placeholder="Search Recipe By Title" />
                 <button  className="py-4 px-8 rounded-md bg-[#07332F] font-semibold text-white ml-3 mt-4 lg:mt-0">Search</button>
             </div>
         </div>
